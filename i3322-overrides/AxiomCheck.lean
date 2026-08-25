4c9ce595c758cd8bc0be0c7ab5c9fa14d1d04dc5
Mansehej Singh <mansehej@gmail.com>
2026-08-25 15:53:11 +0100

    I3322 clean core: exhaustive axiom print file

---
+++ i3322-overrides/AxiomCheck.lean
@@ -0,0 +1,50 @@
+import I3322
+
+#print axioms I3322.pv_eq_cg_after_swaps
+#print axioms I3322.deterministic_bits_local_bound
+#print axioms I3322.deterministic_local_bound
+#print axioms I3322.allZero_saturates
+#print axioms I3322.allOne_saturates
+#print axioms I3322.missingAliceMarginal_breaks_local_bound
+
+#print axioms I3322.Mat2.reflection_mul_self
+#print axioms I3322.Mat2.projector_mul_self
+#print axioms I3322.Mat2.wrong_sign_not_reflection
+#print axioms I3322.scalar_observable_expansion
+#print axioms I3322.exact_rational_witness_above_quarter
+
+#print axioms I3322.responseProjection_is_effect
+#print axioms I3322.diagonal_objective_le_response
+#print axioms I3322.halmosBlock_mul_self
+#print axioms I3322.halmosBlock_compression
+#print axioms I3322.zero_offDiagonal_halmos_not_projector
+
+#print axioms I3322.reflectedQuadratic_sos_identity
+#print axioms I3322.reflected_product_quadratic_nonnegative
+#print axioms I3322.reflected_cross_term_bound
+#print axioms I3322.reflected_interval_to_determinant
+#print axioms I3322.goldenUpper_lt_one_third
+#print axioms I3322.missing_reflected_product_counterexample
+
+#print axioms I3322.atomic_pair_lower_bound
+#print axioms I3322.atomic_pair_contact_value
+#print axioms I3322.atomic_pair_reflection
+#print axioms I3322.two_point_countermonotone
+#print axioms I3322.same_order_pairing_strict_counterexample
+
+#print axioms I3322.extensionMargin_positive
+#print axioms I3322.extensionMargin_at_one_third
+#print axioms I3322.stationarity_propagates_previous_label
+#print axioms I3322.jacobi_reverse_step
+#print axioms I3322.finite_geometric_chain_zero
+#print axioms I3322.dropping_boundary_allows_nonzero_geometric_chain
+
+#print axioms I3322.spectral_union_card_le_sum
+#print axioms I3322.finite_summary_strictly_below
+#print axioms I3322.finite_summary_support_card_bound
+#print axioms I3322.max_instead_of_sum_counterexample
+
+#print axioms I3322.conditional_finite_nonattainment
+#print axioms I3322.conditional_maximizing_chain
+#print axioms I3322.incomplete_reflection_counterexample
+#print axioms I3322.negative_control_bundle
